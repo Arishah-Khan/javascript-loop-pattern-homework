12ecfac806672a2185183fa3d54c076f1ff6dd98
Arishah-Khan <arishahsanaullah78@gmail.com>
2024-06-09 16:49:40 +0500

    javascript loop pattern homework

---
+++ main.js
@@ -0,0 +1,111 @@
+// Right Angle Triangle Pattern
+
+// for(let i = 1 ; i <= 5 ; i++){
+//     for(let j = 1 ; j <= i ; j++){
+//         document.write("*");
+//     }
+
+//     document.write("<br>")
+// }
+
+// user find:
+
+// var userPattern = +prompt("Enter the number of rows for the triangle pattern:")
+
+// for(var i = 1 ; i <= userPattern ; i++){
+//     for (var j = 1 ; j <= i ; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+// Mirror Right Angle Triangle Pattern:
+
+// for(var i = userPattern ; i>=1 ; i--){
+//     for(var j = 1 ; j < i ; j++){
+//         document.write("&nbsp;&nbsp")
+//     }
+//     for(var k = userPattern ; k >= i ; k--){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+// Inverted Right Angle Pattern
+
+// for(var i = userPattern ; i>=1 ; i--){
+//     for(var j = 1 ; j<=i ; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+
+// Complete Pyramid Pattern
+
+// for(i = userPattern ; i >= 1 ; i-- ){
+
+//     for(j = 1 ; j <i ; j++ ){
+//         document.write("&nbsp")
+//     }
+
+//     for(k = userPattern ; k >=i ; k-- ){
+//         document.write("*")
+//     }
+
+//     document.write("<br>")
+
+
+// }
+
+// reverse pyramid:
+
+
+// for(i = 1 ; i <= userPattern ; i++ ){
+
+//     for(j = 1 ; j <i ; j++ ){
+//         document.write("&nbsp")
+//     }
+
+//     for(k = userPattern ; k >=i ; k-- ){
+//         document.write("*")
+//     }
+
+//     document.write("<br>")
+// }
+
+// number pattern
+
+// for (var i = 1; i <= 4; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write(j + " ")
+//     }
+
+//     document.write("<br>")
+
+// }
+
+// for (var i = 1; i <= 4; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write(i + " ")
+//     }
+
+//     document.write("<br>")
+
+// }
+
+
+// var userPattern = +prompt("Enter the number of rows for the triangle pattern:")
+
+
+// var count = 1;
+
+// for (var i = 1; i <= userPattern; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write(count + " ")
+//         count ++
+//     }
+
+//     document.write("<br>")
+
+// }
